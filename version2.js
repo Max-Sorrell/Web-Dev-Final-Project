6315e3d77ccebb3456f8fc737df27e72fade8162
Max-Sorrell <115995570+Max-Sorrell@users.noreply.github.com>
2023-05-11 23:54:35 +0100

    bvh

---
+++ version2.js
@@ -0,0 +1,11 @@
+var myIndex = 0;
+
+
+
+function myFunction() {
+    var i;
+    var x = document.getElementById("Header");
+    for (i = 0; i < 1000; i++) {
+        x.style.opacity = i + "%";
+      }
+  }
